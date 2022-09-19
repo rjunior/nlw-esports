ff0ab4686b27e36fcccf351705f2f725b0bce1d5
Renato Guilherme Martins Júnior <renato.martins@neoway.com.br>
2022-09-18 21:06:42 -0300

    all projects

---
+++ mobile/App.tsx
@@ -0,0 +1,63 @@
+import { useRef, useEffect } from 'react'
+import { StatusBar } from 'react-native'
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_600SemiBold,
+  Inter_700Bold,
+  Inter_900Black
+} from '@expo-google-fonts/inter';
+import { Subscription } from 'expo-modules-core';
+import * as Notifications from 'expo-notifications';
+
+import { Routes } from './src/routes'
+import { Background } from './src/components/Background';
+import { Loading } from './src/components/Loading';
+
+import './src/services/notificationConfigs';
+import { getPushNotificationToken } from './src/services/getPushNotificationToken';
+
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_600SemiBold,
+    Inter_700Bold,
+    Inter_900Black
+  });
+
+  const getPushNotificationListener = useRef<Subscription>();
+  const responsePushNotificationListener = useRef<Subscription>();
+
+  useEffect(() => {
+    getPushNotificationToken();
+  })
+
+  useEffect(() => {
+    getPushNotificationListener.current = Notifications.addNotificationReceivedListener(notification => {
+      console.log(notification)
+    });
+
+    responsePushNotificationListener.current = Notifications.addNotificationResponseReceivedListener(response => {
+      console.log(response)
+    });
+
+    return () => {
+      if (getPushNotificationListener.current && responsePushNotificationListener.current) {
+        Notifications.removeNotificationSubscription(getPushNotificationListener.current)
+        Notifications.removeNotificationSubscription(responsePushNotificationListener.current)
+      }
+    };
+  }, [])
+
+  return (
+    <Background>
+      <StatusBar 
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      {fontsLoaded ? <Routes /> : <Loading />}
+
+    </Background>
+  );
+}
